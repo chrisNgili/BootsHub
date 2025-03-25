4c56f50c578d9a0241d8cb5097c9336f48bbebea
chrisngili <ngilichristopher@gmail.com>
2025-03-25 15:31:53 +0300

    js linked

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-function oneBoot(boot){
-    let card = document.createElement('li')
-    card.className='card'
+function oneBoot(boot) {
+    let card = document.createElement('li');
+    card.className = 'card';
     card.innerHTML = `
-    <img src ="${boot.image}">
+        <img src="${boot.image}" alt="${boot.model}">
+        <h3>${boot.model} </h3>
+        <p>Price: $${boot.price}</p>
+        <p>Sizes: ${boot.sizes.join(', ')}</p>
+        <p>${boot.description}</p>
+    `;
 
- 
-   `
-   document.querySelector('#nike-list').appendChild(card)
-
+    
+    if (boot.brand.toLowerCase() === "nike") {
+        document.querySelector('#nike-list').appendChild(card);
+    } else if (boot.brand.toLowerCase() === "adidas") {
+        document.querySelector('#adidas-list').appendChild(card);
+    } else if (boot.brand.toLowerCase() === "puma") {
+        document.querySelector('#puma-list').appendChild(card);
+    }
 }
 
-
-
-
-function getBoots(){
+function getBoots() {
     fetch("https://phase1-project-data.onrender.com/boots")
-    .then(res => res.json())
-    .then(boots =>boots.forEach(boot => oneBoot(boot)))
+        .then(res => res.json())
+        .then(boots => boots.forEach(boot => oneBoot(boot)))  
 }
 
-function start(){
-    getBoots()
-   // boots.forEach(boot => oneBoot(boot))
+function start() {
+    getBoots();  
 }
-start()
+
+start();
