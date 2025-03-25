de659f0dc02100c2d76144616e5f89c928feccdb
chrisngili <ngilichristopher@gmail.com>
2025-03-25 15:19:34 +0300

    adding lists

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+function oneBoot(boot){
+    let card = document.createElement('li')
+    card.className='card'
+    card.innerHTML = `
+    <img src ="${boot.image}">
+
+ 
+   `
+   document.querySelector('#nike-list').appendChild(card)
+
+}
+
+
+
+
 function getBoots(){
     fetch("https://phase1-project-data.onrender.com/boots")
     .then(res => res.json())
-    .then(data =>console.log(data))
+    .then(boots =>boots.forEach(boot => oneBoot(boot)))
 }
+
+function start(){
+    getBoots()
+   // boots.forEach(boot => oneBoot(boot))
+}
+start()
